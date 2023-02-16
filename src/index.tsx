f0f4fdb4e3aee196639049c642dbd19cb7528155
balahasan.35@gmail.com <balahasan.35@gmail.com>
2023-02-16 11:51:51 +0300

    edited app name

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "store";
-import App from "app";
+import App from "App";
 import "styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
